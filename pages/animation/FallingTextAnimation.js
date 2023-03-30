5e269511d10e46ecf41190452bca59e33695e764
William <william.deazevedopro@gmail.com>
2023-03-30 12:14:04 +0200

    bug textanimation vercel v1

--- pages/animation/FallingTextAnimation.js
+++ pages/animation/FallingTextAnimation.js
@@ -8,9 +8,12 @@ const FallingTextAnimation = ({ text, delay = 0.15, startDelay = 0 }) => {
     setAnimationClass('animate');
   }, []);
 
+  // Ajouter une vérification pour s'assurer que 'text' est défini
+  const words = text ? text.split(' ') : [];
+
   return (
     <div className=" text-xl flex-wrap space-x-2 font-bold md:flex xs:text-md md:mt-2 text-center text-md md:text-2xl lg:text-3xl xl:text-6xl sm:text-2xl">
-      {text.split(' ').map((word, i) => (
+      {words.map((word, i) => (
         <span
           key={i}
           className={`inline-block opacity-0 transform-gpu -translate-y-24 blur-md ${
