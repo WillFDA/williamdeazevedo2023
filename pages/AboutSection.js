550b16bfa5fb27ee17ce8c1cfd0d46abc08ef999
William <william.deazevedopro@gmail.com>
2023-03-30 20:26:59 +0200

    supression clamp et aboutcube hardcoded

--- pages/AboutSection.js
+++ pages/AboutSection.js
@@ -1,4 +1,5 @@
 import React from "react"
+import AboutCube from "./components/AboutCube"
 export default function AboutSection() {
     return (
         <>
@@ -11,11 +12,7 @@ export default function AboutSection() {
 
                 </div>
                 <div className="flex">
-                    <div className="rounded-xl bg-[#060606] p-5 flex flex-col text-center h-[187px] justify-between border border-white/10">
-                        <span className="text-white text-2xl">2017</span>
-                        <p className="text-[#8D0C0C]">BAC PRO<br />Production<br />graphique</p>
-                        <span className="text-gray-600">Lycée<br />Claude Garamont</span>
-                    </div>
+                    <AboutCube />
                 </div>
                </div>
             </div>
