9d3b5a4f701a96d4ff821825af7f28aaa76616ef
William <william.deazevedopro@gmail.com>
2023-05-31 22:57:55 +0200

    ''''''' ta capté

--- src/sections/SectionHero.tsx
+++ src/sections/SectionHero.tsx
@@ -8,10 +8,10 @@ import IconsHero from "@/components/IconsHero"
 function SectionHero() {
     return (
         <section className="hero__section">
-            <AnimationFallingHero text='Salut, moi c’est William Ferreira !' />
-            <AnimationFallingHero startDelay={0.5} text='Front-end junior & designer d’interface' />
+            <AnimationFallingHero text='Salut, moi c&apos;est William Ferreira !' />
+            <AnimationFallingHero startDelay={0.5} text='Front-end junior & designer d&apos;interface' />
             <p className="hero__text">
-                Autodidacte de 25 ans, je suis passionné par le développement web, le design et l'accessibilité depuis mes 16 ans
+                Autodidacte de 25 ans, je suis passionné par le développement web, le design et l&apos;accessibilité depuis mes 16 ans
             </p>
             <IconsHero />
         </section>
