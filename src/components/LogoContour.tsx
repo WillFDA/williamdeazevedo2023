2a9a5f70516d7ac64565ab29e3caf8ff62b04261
William <william.deazevedopro@gmail.com>
2023-06-02 12:42:56 +0200

    clipPath strokeWidth

--- src/components/LogoContour.tsx
+++ src/components/LogoContour.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 const LogoContour = () => {
   return (
     <svg className='logo-svg' width="1270" height="875" viewBox="0 0 1270 875" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <g clip-path="url(#clip0_1048_64)">
-        <path className="path1" d="M801.436 0.5H1269.13L1033.94 403.736L801.436 0.5Z" stroke="#D9D9D9" stroke-width="1"/>
-        <path className="path2" d="M542.596 498.017L542.545 498.118L392.081 237.251L392.183 237.099L392.027 236.829L255.575 0.5H557.603L911.332 613.817L759.457 874.004L543.059 498.819L542.596 498.017Z" stroke="#D9D9D9" stroke-width="1"/>
-        <path className="path3" d="M-3.11301 498.016L-3.16385 498.117L-153.628 237.251L-153.526 237.099L-153.682 236.829L-290.134 0.5H11.8941L365.624 613.817L213.798 874.004L-2.65002 498.819L-3.11301 498.016Z" stroke="#D9D9D9" stroke-width="1"/>
+    <g clipPath="url(#clip0_1048_64)">
+        <path className="path1" d="M801.436 0.5H1269.13L1033.94 403.736L801.436 0.5Z" stroke="#D9D9D9"/>
+        <path className="path2" d="M542.596 498.017L542.545 498.118L392.081 237.251L392.183 237.099L392.027 236.829L255.575 0.5H557.603L911.332 613.817L759.457 874.004L543.059 498.819L542.596 498.017Z" stroke="#D9D9D9" />
+        <path className="path3" d="M-3.11301 498.016L-3.16385 498.117L-153.628 237.251L-153.526 237.099L-153.682 236.829L-290.134 0.5H11.8941L365.624 613.817L213.798 874.004L-2.65002 498.819L-3.11301 498.016Z" stroke="#D9D9D9" />
     </g>
     <defs>
         <clipPath id="clip0_1048_64">
