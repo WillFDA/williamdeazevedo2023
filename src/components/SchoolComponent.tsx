df27953ad33e45a35b7c9ecdafdbc1a60587d462
William <william.deazevedopro@gmail.com>
2023-06-15 14:48:34 +0200

    Section a propos done
    Il manque le footer et le menu dropdown de la navbar sinon goodo

---
+++ src/components/SchoolComponent.tsx
@@ -0,0 +1,119 @@
+'use client'
+import React, { useState } from 'react'
+import {motion} from 'framer-motion';
+// le CSS de se fichier est dans SectionAPropos
+
+const SchoolComponent = () => {
+    const [visibleContent, setVisibleContent] = useState('2023');
+  return (
+    <div className='about-container'>
+            <div className="about-container-list">
+            <button 
+                    className={`about-container-list-item ${visibleContent === '2023' ? 'about-active' : ''}`} 
+                    id='tab-0' 
+                    tabIndex={0} 
+                    onClick={() => setVisibleContent('2023')}>
+                    <span>2023</span>
+                </button> 
+                <button 
+                    className={`about-container-list-item ${visibleContent === '2020' ? 'about-active' : ''}`} 
+                    id='tab-1' 
+                    tabIndex={1} 
+                    onClick={() => setVisibleContent('2020')}>
+                    <span>2020</span>
+                </button> 
+                <button 
+                    className={`about-container-list-item ${visibleContent === '2019' ? 'about-active' : ''}`} 
+                    id='tab-2' 
+                    tabIndex={2} 
+                    onClick={() => setVisibleContent('2019')}>
+                    <span>2019</span>
+                </button> 
+                <button 
+                    className={`about-container-list-item ${visibleContent === '2017' ? 'about-active' : ''}`} 
+                    id='tab-3' 
+                    tabIndex={3} 
+                    onClick={() => setVisibleContent('2017')}>
+                    <span>2017</span>
+                </button> 
+                <div className="about-container-list-highlight"></div>
+            </div>
+            <div className="about-container-content">
+                {visibleContent === '2023' && (
+                    <motion.div
+                    initial={{ opacity: 0}}
+                    animate={{ opacity: visibleContent === '2023' ? 1 : 0, y: visibleContent === '2023' ? 0 : 50 }}
+                    transition={{ duration: 0.5 }}
+                    className='about-content'
+                  >
+                      <h3 className='about-content-title'>Formation Intégrateur Web <a id='openclassrooms' className='about-content-link' target='_blank' href="https://openclassrooms.com/fr/paths/594-integrateur-web">@OpenClassrooms</a></h3>
+                      <p className='about-content-subtitle'>Septembre 2022 - Juin 2023</p>
+                      <div>
+                        <ul className='about-content-ul'>
+                          <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, nemo reprehenderit hic laboriosam iusto dicta.</li>
+                          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni praesentium nulla dolor?</li>
+                          <li>Lorem ipsum dolor sit amet.</li>
+                        </ul>
+                      </div>
+                  </motion.div>
+                )}
+                {visibleContent === '2020' && (
+                    <motion.div
+                    initial={{ opacity: 0}}
+                    animate={{ opacity: visibleContent === '2020' ? 1 : 0, y: visibleContent === '2020' ? 0 : 50 }}
+                    transition={{ duration: 0.5 }}
+                    className='about-content'
+                  >
+                    <h3 className='about-content-title'>Web design <a id='icanschool' className='about-content-link' target='_blank' href="https://openclassrooms.com/fr/paths/594-integrateur-web">@ICAN</a></h3>
+                    <p className='about-content-subtitle'>Septembre 2019 - Janvier 2020</p>
+                    <div>
+                      <ul className='about-content-ul'>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, nemo reprehenderit hic laboriosam iusto dicta.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni praesentium nulla dolor?</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                      </ul>
+                    </div>
+                    </motion.div>
+                )}
+                {visibleContent === '2019' && (
+                    <motion.div
+                    initial={{ opacity: 0}}
+                    animate={{ opacity: visibleContent === '2019' ? 1 : 0, y: visibleContent === '2019' ? 0 : 50 }}
+                    transition={{ duration: 0.5 }}
+                    className='about-content'
+                  >
+                    <h3 className='about-content-title'>BTS Etude Etude et realisation d'un projet de communication<br /><a id='Estienne' className='about-content-link' target='_blank' href="http://www.ecole-estienne.paris/formation/bts-industries-graphiques-etudes-de-realisation-de-produits-de-communication/presentation/">@Ecole Estienne</a></h3>
+                    <p className='about-content-subtitle'>Septembre 2017 - Aout 2019</p>
+                    <div>
+                      <ul className='about-content-ul'>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, nemo reprehenderit hic laboriosam iusto dicta.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni praesentium nulla dolor?</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                      </ul>
+                    </div>
+                    </motion.div>
+                )}
+                {visibleContent === '2017' && (
+                    <motion.div
+                    initial={{ opacity: 0}}
+                    animate={{ opacity: visibleContent === '2017' ? 1 : 0, y: visibleContent === '2017' ? 0 : 50 }}
+                    transition={{ duration: 0.5 }}
+                    className='about-content'
+                  >
+                    <h3 className='about-content-title'>Bac PRO Réalisation d'un Projet imprimé et Plurimédia<br /><a id='Estienne' className='about-content-link' target='_blank' href="https://www.lyceegaramont.fr/bac-pro-rpip/">Lycée Claude Garamont</a></h3>
+                    <p className='about-content-subtitle'>Septembre 2017 - Septembre 2014</p>
+                    <div>
+                      <ul className='about-content-ul'>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, nemo reprehenderit hic laboriosam iusto dicta.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni praesentium nulla dolor?</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                      </ul>
+                    </div>
+                    </motion.div>
+                )}
+            </div>
+        </div>
+  )
+}
+
+export default SchoolComponent
--- src/sections/SectionAPropos.tsx
+++ src/sections/SectionAPropos.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import Image from 'next/image'
 import {motion} from 'framer-motion';
+import SchoolComponent from '@/components/SchoolComponent';
 
 const SectionAPropos = () => {
   return (
@@ -18,6 +19,7 @@ const SectionAPropos = () => {
     >
         <h3 className='about-title'>A Propos de moi</h3>
     </motion.div>
+    
     <motion.div
       className='about-box'
       initial="hidden"
@@ -29,27 +31,36 @@ const SectionAPropos = () => {
         hidden: { opacity: 0, y: 0 },
       }}
     >
-        <div className="about-image">
-        <Image
-            src="/william-coeur.jpg" 
-            alt="Photo de William vue de face et un font végétale verdoyant" 
-            width={400}
-            height={400} 
-            style={{ objectFit: 'cover' }}
+          <div className="about-image">
+            <Image
+                src="/william-coeur.jpg" 
+                alt="Photo de William vue de face et un font végétale verdoyant" 
+                style={{objectFit: "cover"}}
+                fill={true}
             />
-        </div>
-        <div className='about-container'>
-            <div className="about-container-list">
-                <button className='about-container-list-item' id='tab-0' tabIndex={0}>
-                    <span>2019</span>
-                </button> 
-                <div className="about-container-list-highlight"></div>
-            </div>
-            <div className="about-container-content">
+          </div>
+        <aside className='about-aside'>
+          <div className='about-me'>
+            <h3>Mon histoire</h3>
+            <div className="about-me-paragraph">
+            <p>Passionné par l'informatique et la créativité visuelle depuis mon jeune âge,
+                j'ai commencé à explorer l'univers de l'infographie à seulement 11 ans.
+                </p>
+                <p>Mon voyage a commencé par la modélisation 3D, le montage vidéo et le traitement d'image pour 
+                créer des affiches et d'autres matériaux graphiques. J'ai ensuite approfondi mes connaissances 
+                dans ces domaines en poursuivant des études universitaires en impression et plurimédia. </p>
 
-            </div>
-        </div>
+                <p>Cependant, je n'ai jamais cessé d'être intrigué par le potentiel de l'informatique. 
+                C'est pourquoi en 2022, j'ai décidé de m'orienter vers le développement web, 
+                en commençant une formation d'intégrateur web. Aujourd'hui, je suis fier de dire que je suis diplômé,
+                avec plusieurs mois d'expérience dans le domaine, prêt à apporter ma passion et mes compétences à de nouveaux défis et projets.
+                 </p>
+              </div>
+          </div>
+        <SchoolComponent />
+        </aside>
     </motion.div>
+    
     
     </section>
   )
