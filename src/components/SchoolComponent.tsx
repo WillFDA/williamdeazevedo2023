db8d04d7f679c6cfab92df1e9abb8beed5837aaa
William <william.deazevedopro@gmail.com>
2023-06-15 15:57:40 +0200

    apostrophe v2

--- src/components/SchoolComponent.tsx
+++ src/components/SchoolComponent.tsx
@@ -9,47 +9,47 @@ const SchoolComponent = () => {
     <div className='about-container'>
             <div className="about-container-list">
             <button 
-                    className={`about-container-list-item ${visibleContent === '2023' ? 'about-active' : ''}`} 
-                    id='tab-0' 
+                    className={`about-container-list-item ${visibleContent === "2023" ? "about-active" : ""}`} 
+                    id="tab-0" 
                     tabIndex={0} 
-                    onClick={() => setVisibleContent('2023')}>
+                    onClick={() => setVisibleContent("2023")}>
                     <span>2023</span>
                 </button> 
                 <button 
-                    className={`about-container-list-item ${visibleContent === '2020' ? 'about-active' : ''}`} 
-                    id='tab-1' 
+                    className={`about-container-list-item ${visibleContent === "2020" ? "about-active" : ""}`} 
+                    id="tab-1" 
                     tabIndex={1} 
-                    onClick={() => setVisibleContent('2020')}>
+                    onClick={() => setVisibleContent("2020")}>
                     <span>2020</span>
                 </button> 
                 <button 
-                    className={`about-container-list-item ${visibleContent === '2019' ? 'about-active' : ''}`} 
-                    id='tab-2' 
+                    className={`about-container-list-item ${visibleContent === "2019" ? "about-active" : ""}`} 
+                    id="tab-2" 
                     tabIndex={2} 
-                    onClick={() => setVisibleContent('2019')}>
+                    onClick={() => setVisibleContent("2019")}>
                     <span>2019</span>
                 </button> 
                 <button 
-                    className={`about-container-list-item ${visibleContent === '2017' ? 'about-active' : ''}`} 
-                    id='tab-3' 
+                    className={`about-container-list-item ${visibleContent === "2017" ? "about-active" : ""}`} 
+                    id="tab-3" 
                     tabIndex={3} 
-                    onClick={() => setVisibleContent('2017')}>
+                    onClick={() => setVisibleContent("2017")}>
                     <span>2017</span>
                 </button> 
                 <div className="about-container-list-highlight"></div>
             </div>
             <div className="about-container-content">
-                {visibleContent === '2023' && (
+                {visibleContent === "2023" && (
                     <motion.div
                     initial={{ opacity: 0}}
-                    animate={{ opacity: visibleContent === '2023' ? 1 : 0, y: visibleContent === '2023' ? 0 : 50 }}
+                    animate={{ opacity: visibleContent === "2023" ? 1 : 0, y: visibleContent === "2023" ? 0 : 50 }}
                     transition={{ duration: 0.5 }}
                     className='about-content'
                   >
-                      <h3 className='about-content-title'>Formation Intégrateur Web <a id='openclassrooms' className='about-content-link' target='_blank' href="https://openclassrooms.com/fr/paths/594-integrateur-web">@OpenClassrooms</a></h3>
-                      <p className='about-content-subtitle'>Septembre 2022 - Juin 2023</p>
+                      <h3 className="about-content-title">Formation Intégrateur Web <a id="openclassrooms" className="about-content-link" target="_blank" href="https://openclassrooms.com/fr/paths/594-integrateur-web">@OpenClassrooms</a></h3>
+                      <p className="about-content-subtitle">Septembre 2022 - Juin 2023</p>
                       <div>
-                        <ul className='about-content-ul'>
+                        <ul className="about-content-ul">
                           <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, nemo reprehenderit hic laboriosam iusto dicta.</li>
                           <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni praesentium nulla dolor?</li>
                           <li>Lorem ipsum dolor sit amet.</li>
@@ -57,17 +57,17 @@ const SchoolComponent = () => {
                       </div>
                   </motion.div>
                 )}
-                {visibleContent === '2020' && (
+                {visibleContent === "2020" && (
                     <motion.div
                     initial={{ opacity: 0}}
-                    animate={{ opacity: visibleContent === '2020' ? 1 : 0, y: visibleContent === '2020' ? 0 : 50 }}
+                    animate={{ opacity: visibleContent === "2020" ? 1 : 0, y: visibleContent === "2020" ? 0 : 50 }}
                     transition={{ duration: 0.5 }}
-                    className='about-content'
+                    className="about-content"
                   >
-                    <h3 className='about-content-title'>Web design <a id='icanschool' className='about-content-link' target='_blank' href="https://openclassrooms.com/fr/paths/594-integrateur-web">@ICAN</a></h3>
-                    <p className='about-content-subtitle'>Septembre 2019 - Janvier 2020</p>
+                    <h3 className="about-content-title">Web design <a id="icanschool" className="about-content-link" target="_blank" href="https://openclassrooms.com/fr/paths/594-integrateur-web">@ICAN</a></h3>
+                    <p className="about-content-subtitle">Septembre 2019 - Janvier 2020</p>
                     <div>
-                      <ul className='about-content-ul'>
+                      <ul className="about-content-ul">
                         <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, nemo reprehenderit hic laboriosam iusto dicta.</li>
                         <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni praesentium nulla dolor?</li>
                         <li>Lorem ipsum dolor sit amet.</li>
@@ -75,17 +75,17 @@ const SchoolComponent = () => {
                     </div>
                     </motion.div>
                 )}
-                {visibleContent === '2019' && (
+                {visibleContent === "2019" && (
                     <motion.div
                     initial={{ opacity: 0}}
-                    animate={{ opacity: visibleContent === '2019' ? 1 : 0, y: visibleContent === '2019' ? 0 : 50 }}
+                    animate={{ opacity: visibleContent === "2019" ? 1 : 0, y: visibleContent === "2019" ? 0 : 50 }}
                     transition={{ duration: 0.5 }}
-                    className='about-content'
+                    className="about-content"
                   >
-                    <h3 className='about-content-title'>BTS Etude Etude et realisation d'un projet de communication<br /><a id='Estienne' className='about-content-link' target='_blank' href="http://www.ecole-estienne.paris/formation/bts-industries-graphiques-etudes-de-realisation-de-produits-de-communication/presentation/">@Ecole Estienne</a></h3>
-                    <p className='about-content-subtitle'>Septembre 2017 - Aout 2019</p>
+                    <h3 className="about-content-title">BTS Etude Etude et realisation d&apos;un projet de communication<br /><a id="Estienne" className="about-content-link" target="_blank" href="http://www.ecole-estienne.paris/formation/bts-industries-graphiques-etudes-de-realisation-de-produits-de-communication/presentation/">@Ecole Estienne</a></h3>
+                    <p className="about-content-subtitle">Septembre 2017 - Aout 2019</p>
                     <div>
-                      <ul className='about-content-ul'>
+                      <ul className="about-content-ul">
                         <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, nemo reprehenderit hic laboriosam iusto dicta.</li>
                         <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni praesentium nulla dolor?</li>
                         <li>Lorem ipsum dolor sit amet.</li>
@@ -93,17 +93,17 @@ const SchoolComponent = () => {
                     </div>
                     </motion.div>
                 )}
-                {visibleContent === '2017' && (
+                {visibleContent === "2017" && (
                     <motion.div
                     initial={{ opacity: 0}}
-                    animate={{ opacity: visibleContent === '2017' ? 1 : 0, y: visibleContent === '2017' ? 0 : 50 }}
+                    animate={{ opacity: visibleContent === "2017" ? 1 : 0, y: visibleContent === "2017" ? 0 : 50 }}
                     transition={{ duration: 0.5 }}
                     className='about-content'
                   >
-                    <h3 className='about-content-title'>Bac PRO Réalisation d'un Projet imprimé et Plurimédia<br /><a id='Estienne' className='about-content-link' target='_blank' href="https://www.lyceegaramont.fr/bac-pro-rpip/">Lycée Claude Garamont</a></h3>
-                    <p className='about-content-subtitle'>Septembre 2017 - Septembre 2014</p>
+                    <h3 className="about-content-title">Bac PRO Réalisation d&apos;un Projet imprimé et Plurimédia<br /><a id="Estienne" className="about-content-link" target="_blank" href="https://www.lyceegaramont.fr/bac-pro-rpip/">Lycée Claude Garamont</a></h3>
+                    <p className="about-content-subtitle">Septembre 2017 - Septembre 2014</p>
                     <div>
-                      <ul className='about-content-ul'>
+                      <ul className="about-content-ul">
                         <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, nemo reprehenderit hic laboriosam iusto dicta.</li>
                         <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni praesentium nulla dolor?</li>
                         <li>Lorem ipsum dolor sit amet.</li>
--- src/sections/SectionAPropos.tsx
+++ src/sections/SectionAPropos.tsx
@@ -1,12 +1,12 @@
-'use client'
-import React from 'react'
-import Image from 'next/image'
-import {motion} from 'framer-motion';
-import SchoolComponent from '@/components/SchoolComponent';
+"use client"
+import React from "react"
+import Image from "next/image"
+import {motion} from "framer-motion";
+import SchoolComponent from "@/components/SchoolComponent";
 
 const SectionAPropos = () => {
   return (
-    <section className='about'>
+    <section className="about">
         <motion.div
       initial="hidden"
       whileInView="visible"
@@ -17,11 +17,11 @@ const SectionAPropos = () => {
         hidden: { opacity: 0, y: 0 },
       }}
     >
-        <h3 className='about-title'>A Propos de moi</h3>
+        <h3 className="about-title">A Propos de moi</h3>
     </motion.div>
     
     <motion.div
-      className='about-box'
+      className="about-box"
       initial="hidden"
       whileInView="visible"
       viewport={{ once: true }}
@@ -39,23 +39,23 @@ const SectionAPropos = () => {
                 fill={true}
             />
           </div>
-        <aside className='about-aside'>
-          <div className='about-me'>
+        <aside className="about-aside">
+          <div className="about-me">
             <h3>Mon histoire</h3>
             <div className="about-me-paragraph">
-            <p>Passionné par l&apos;informatique et la créativité visuelle depuis mon jeune âge,
-                j&apos;ai commencé à explorer l&apos;univers de l&apos;infographie à seulement 11 ans.
-                </p>
-                <p>Mon voyage a commencé par la modélisation 3D, le montage vidéo et le traitement d'image pour 
-                créer des affiches et d&apos;autres matériaux graphiques. J&apos;ai ensuite approfondi mes connaissances 
-                dans ces domaines en poursuivant des études universitaires en impression et plurimédia. </p>
+  <p>Passionné par l&apos;informatique et la créativité visuelle depuis mon jeune âge,
+  j&apos;ai commencé à explorer l&apos;univers de l&apos;infographie à seulement 11 ans.
+  </p>
+  <p>Mon voyage a commencé par la modélisation 3D, le montage vidéo et le traitement d'image pour 
+  créer des affiches et d&apos;autres matériaux graphiques. J&apos;ai ensuite approfondi mes connaissances 
+  dans ces domaines en poursuivant des études universitaires en impression et plurimédia. </p>
 
-                <p>Cependant, je n&apos;ai jamais cessé d'être intrigué par le potentiel de l&apos;informatique. 
-                C&apos;est pourquoi en 2022, j&apos;ai décidé de m&apos;orienter vers le développement web, 
-                en commençant une formation d'intégrateur web. Aujourd&apos;hui, je suis fier de dire que je suis diplômé,
-                avec plusieurs mois d&apos;expérience dans le domaine, prêt à apporter ma passion et mes compétences à de nouveaux défis et projets.
-                 </p>
-              </div>
+  <p>Cependant, je n&apos;ai jamais cessé d'être intrigué par le potentiel de l&apos;informatique. 
+  C&apos;est pourquoi en 2022, j&apos;ai décidé de m&apos;orienter vers le développement web, 
+  en commençant une formation d'intégrateur web. Aujourd&apos;hui, je suis fier de dire que je suis diplômé,
+  avec plusieurs mois d&apos;expérience dans le domaine, prêt à apporter ma passion et mes compétences à de nouveaux défis et projets.
+  </p>
+</div>
           </div>
         <SchoolComponent />
         </aside>
