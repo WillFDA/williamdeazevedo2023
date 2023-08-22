1acda943889bb197162bf7eb69d74d0b783132b9
William <william.deazevedopro@gmail.com>
2023-08-22 17:32:41 +0200

    Update SectionTechnologies.tsx

--- src/sections/SectionTechnologies.tsx
+++ src/sections/SectionTechnologies.tsx
@@ -1,56 +1,63 @@
-'use client'
+"use client";
 import React from "react";
 import { IoLogoCss3 } from "react-icons/io";
 import { FaReact, FaSass } from "react-icons/fa";
-import {SiTypescript, SiAdobephotoshop, SiAdobeillustrator, SiFigma, SiAdobeaftereffects } from "react-icons/si";
+import {
+  SiTypescript,
+  SiAdobephotoshop,
+  SiAdobeillustrator,
+  SiFigma,
+  SiAdobeaftereffects,
+  SiRedux,
+  SiTailwindcss,
+} from "react-icons/si";
 import { TbBrandNextjs } from "react-icons/tb";
 import TechnologiesBox from "@/components/TechnologiesBox";
 // import AnimationTechnologiesBox from "@/animations/AnimationTechnologiesBox";
-import {motion} from 'framer-motion';
+import { motion } from "framer-motion";
 
 export default function SectionTechnologies() {
   return (
     <>
       <div id="technos" className="mes-techno">
-      <motion.div
-                initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true }}
-                transition={{ duration: 0.6 }}
-                variants={{
-                  visible: { opacity: 1, y: -50 },
-                  hidden: { opacity: 0, y: 0 },
-                }}
-              >
-        <h1 className="mes-techno__title">
-          Mes technologies
-        </h1>
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.6 }}
+          variants={{
+            visible: { opacity: 1, y: -50 },
+            hidden: { opacity: 0, y: 0 },
+          }}
+        >
+          <h1 className="mes-techno__title">Mes technologies</h1>
         </motion.div>
 
         <motion.div
-                initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true }}
-                transition={{ duration: 0.9 }}
-                variants={{
-                  visible: { opacity: 1, y: -50 },
-                  hidden: { opacity: 0, y: 0 },
-                }}
-              >
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.9 }}
+          variants={{
+            visible: { opacity: 1, y: -50 },
+            hidden: { opacity: 0, y: 0 },
+          }}
+        >
           <div className="mes-techno__content">
             <TechnologiesBox
               title="Front-end"
-              paragraph="Technologies que j&apos;ai appris au cours de ma formation et en autodidacte."
+              paragraph="Technologies que j'ai appris au cours de ma formation et en autodidacte."
               icons={[
                 { component: <FaReact />, color: "#61DBFB" },
-                { component: <FaSass />, color: "#CD669A" },
                 { component: <SiTypescript />, color: "#06B6D4" },
                 { component: <TbBrandNextjs />, color: "#000000" },
+                { component: <SiRedux />, color: "##764ABC" },
+                { component: <SiTailwindcss />, color: "##38BDF8" },
               ]}
             />
             <TechnologiesBox
               title="Design"
-              paragraph="Toute les applications que j&apos;ai utilisé lors de mes études et stages."
+              paragraph="Toute les applications que j'ai utilisé lors de mes études et stages."
               icons={[
                 { component: <SiAdobephotoshop />, color: "#4FCCFE" },
                 { component: <SiAdobeillustrator />, color: "#FF992D" },
@@ -59,7 +66,7 @@ export default function SectionTechnologies() {
               ]}
             />
           </div>
-          </motion.div>
+        </motion.div>
       </div>
     </>
   );
